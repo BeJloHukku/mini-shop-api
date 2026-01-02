a8e6dc1012ae79a7e5b578d585fe9308e2fdf01b
BeJloHukku <igoryashamb@gmail.com>
2026-01-02 22:33:39 +0300

    feat: created db_helper and lifespan for fastapi app

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ import uvicorn
 from items_views import router as items_router
 from users.views import router as users_router
 
-app = FastAPI(title="Mini-shop")
+from contextlib import asynccontextmanager
+
+from core.models import Base, db_helper
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+app = FastAPI(lifespan=lifespan, title="Mini-shop")
 app.include_router(items_router)
 app.include_router(users_router)
 
