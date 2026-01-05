8026a2acdd840ca93ebfbdda76f2136fce7f04db
BeJloHukku <igoryashamb@gmail.com>
2026-01-05 15:43:16 +0300

    feat: created several cruds for products table

--- core/models/product.py
+++ core/models/product.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Mapped
 from .base import Base
 
 class Product(Base):
-
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from core.models import Base, db_helper
 async def lifespan(app: FastAPI):
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
     yield
 
 app = FastAPI(lifespan=lifespan, title="Mini-shop")
