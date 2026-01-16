942a56c2135c6e725a34a2c3e27373ae68183ff6
BeJloHukku <igoryashamb@yandex.ru>
2026-01-16 20:40:58 +0300

    feat: create profile model

---
+++ core/models/profile.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins import UserRelationMixin
+
+
+class Profile(Base, UserRelationMixin):
+    _user_back_populates = "profile"
+    _user_id_unique = True
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    second_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None]
