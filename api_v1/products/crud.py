f72ad4f8a51b573da95719495a23cf9c61f03be9
BeJloHukku <igoryashamb@yandex.ru>
2026-01-17 22:46:52 +0300

    style: change style for function get product

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -12,8 +12,13 @@ async def get_products(session: AsyncSession) -> list[Product]:
     products = result.scalars().all()
     return list(products)
 
-async def get_product(session: AsyncSession, product_id: int) -> Product | None:
+
+async def get_product(
+    session: AsyncSession,
+    product_id: int,
+) -> Product | None:
     return await session.get(Product, product_id)
+
 
 async def create_product(session: AsyncSession, product_in: ProductCreate) -> Product:
     product = Product(**product_in.model_dump())
@@ -22,20 +27,22 @@ async def create_product(session: AsyncSession, product_in: ProductCreate) -> Pr
     # await session.refresh(product)
     return product
 
+
 async def update_product(
-        session: AsyncSession,
-        product: Product,
-        product_update: ProductUpdate | ProductPartialUpdate,
-        partial: bool = False,
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate | ProductPartialUpdate,
+    partial: bool = False,
 ) -> Product:
     for key, value in product_update.model_dump(exclude_unset=partial).items():
-         setattr(product, key, value)
+        setattr(product, key, value)
     await session.commit()
     return product
 
+
 async def delete_product(
-        session: AsyncSession,
-        product: Product,
+    session: AsyncSession,
+    product: Product,
 ) -> None:
-     await session.delete(product)
-     await session.commit()
+    await session.delete(product)
+    await session.commit()
