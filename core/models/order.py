3661dcf416a0fdbc4265c1e8774be2522075e753
BeJloHukku <igoryashamb@gmail.com>
2026-01-18 11:42:50 +0300

    feat: create order model

---
+++ core/models/order.py
@@ -0,0 +1,13 @@
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, UTC
+
+from .base import Base
+
+
+class Order(Base):
+    promocode: Mapped[str | None]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.now(UTC),
+    )
